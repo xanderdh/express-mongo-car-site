c8a3a619a0e56b5cf690150d94b561834338d402
den4ik <xander.x_net@mail.ru>
2019-12-22 20:08:22 +0200

    refactor

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,16 +5,16 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   const dbNavList = await Nav.find({});
-  
-  const navList = dbNavList.map(el=> {
+
+  const navList = dbNavList.map(el => {
     const newEl = el;
     newEl.active = false;
-    
+
     return newEl
   });
-  
+
   res.render('pages/index', {
-    title: 'Home page', 
+    title: 'Home page',
     header: {
       hasBodiesLing: true,
       navList
