1ecc71a984107387da447aad21e08c205dac558e
den4ik <xander.x_net@mail.ru>
2020-01-07 15:41:48 +0200

    add manufacturer list render

--- src/routes/admin/carTypes.js
+++ src/routes/admin/carTypes.js
@@ -14,23 +14,35 @@ const genFileName = (fileType) => {
   return `${uuid()}.${fileType.split('/')[1]}`
 };
 
+const getEditCarPageData = async () => {
+  const manufacturerList = await CarManufacturer.find({});
+
+  return {
+    isCarTypesPage: true,
+    manufacturerList: manufacturerList
+  }
+};
+
 router.get('/', isAuthenticated, async (req, res) => {
-  res.render('pages/admin/car_type', {isCarTypesPage: true})
+  res.render('pages/admin/car_type', {
+      ...await getEditCarPageData()
+    }
+  )
 });
 
 router.post('/add-car-manufacture', isAuthenticated, async (req, res) => {
   const name = req.body.name;
-  
-  if(!name) {
+
+  if (!name) {
     return res.render('pages/admin/car_type', {
-      isCarTypesPage: true,
+      ...await getEditCarPageData(),
       isNoName: true,
     })
   }
-  
+
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.render('pages/admin/car_type', {
-      isCarTypesPage: true,
+      ...await getEditCarPageData(),
       isNoFile: true,
       name,
     })
@@ -41,7 +53,7 @@ router.post('/add-car-manufacture', isAuthenticated, async (req, res) => {
 
   if (!isImageMimetype(fileType)) {
     return res.render('pages/admin/car_type', {
-      isCarTypesPage: true,
+      ...await getEditCarPageData(),
       inNotImage: true,
       name,
     })
@@ -50,10 +62,18 @@ router.post('/add-car-manufacture', isAuthenticated, async (req, res) => {
   const fileName = genFileName(fileType);
   const filePath = path.join(__dirname, '../../../', 'public', 'upload', fileName);
 
-  file.mv(filePath, err => {
+  file.mv(filePath, async err => {
     if (err) {
       return res.status(500).send(err)
     }
+
+    const newManufacturer = new CarManufacturer({
+      title: name,
+      manufacturer: name,
+      imgUrl: `/upload/${fileName}`
+    });
+
+    await newManufacturer.save();
 
     res.redirect('/admin/car-types')
   })
